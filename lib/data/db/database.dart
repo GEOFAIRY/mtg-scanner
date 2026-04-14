763edf3325b1d8fdbd3cc0025b01c0da4733abcf
GEOFAIRY <krs19@live.com>
2026-04-14 21:58:36 +1200

    wire drift database and run codegen

---
+++ lib/data/db/database.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
+import 'tables.dart';
+
+part 'database.g.dart';
+
+@DriftDatabase(tables: [Scans, Collection])
+class AppDatabase extends _$AppDatabase {
+  AppDatabase() : super(_open());
+  AppDatabase.forTesting(QueryExecutor e) : super(e);
+
+  @override
+  int get schemaVersion => 1;
+}
+
+LazyDatabase _open() {
+  return LazyDatabase(() async {
+    final dir = await getApplicationDocumentsDirectory();
+    final file = File(p.join(dir.path, 'mtg_scanner.sqlite'));
+    return NativeDatabase.createInBackground(file);
+  });
+}
